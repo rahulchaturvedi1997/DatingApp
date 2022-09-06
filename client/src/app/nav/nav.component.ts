ffba518d148c0f84d675571944b1b036cab28718
rahulchaturvedi1997 <rahulchaturvedi00@gmail.com>
2022-09-06 18:03:39 +0530

    error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,9 +22,6 @@ model : any = {};
   {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    },error => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
